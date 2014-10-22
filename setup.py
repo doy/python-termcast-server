524e5f4fa0603e565eed95a5eff58c0ddebb19a0
Jesse Luehrs <doy@tozt.net>
2014-10-22 12:16:21 -0400

    changelog and version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="termcast_server",
-    version="0.1.1",
+    version="0.1.2",
     description="allow users to broadcast their terminals for others to watch",
     url="https://github.com/doy/python-termcast-server",
     author="Jesse Luehrs",
